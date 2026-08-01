aa4ac221dac6f19332612266460377e6961e911c
grahmharsh-neon <grahmharsh@gmail.com>
2026-08-01 15:38:49 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.NEON_CONFIG = {
-  supabaseUrl: "https://YOUR-PROJECT-ID.supabase.co",
+  supabaseUrl: "https://gcriavosagoteonbtbng.supabase.co",
   supabasePublishableKey: "sb_publishable_oo3z1dyjsy3Toj_Nx0vb_g_MMk8RDqm",
   storageBucket: "product-images",
   adminRedirect: "/admin.html"
